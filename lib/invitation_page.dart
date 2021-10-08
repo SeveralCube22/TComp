e9b425b39ad83a419e90a498d6d2069caa2b6c72
SeveralCube22 <manam.viswadeep@gmail.com>
2021-10-07 19:59:52 -0700

    UI for players in session.

--- lib/invitation_page.dart
+++ lib/invitation_page.dart
@@ -19,6 +19,7 @@ class Invitation extends StatefulWidget {
 class _InvitationState extends State<Invitation> {
   var sessionController = TextEditingController();
   var currId = "";
+  List<Player> players = [];
 
   @override
   Widget build(BuildContext context) {
@@ -75,9 +76,23 @@ class _InvitationState extends State<Invitation> {
                               .child(session)
                               .child("Link")
                               .get();
+                          var playerData = await FirebaseDatabase.instance
+                              .reference()
+                              .child("Sessions")
+                              .child(data.value)
+                              .child("Players")
+                              .get();
+
                           setState(() {
                             sessionController.text = session;
                             currId = data.value;
+                            HashMap<dynamic, dynamic> values = playerData.value;
+                            values.forEach((key, value) {
+                              HashMap<dynamic, dynamic> player = value;
+                              String name = player["Name"];
+                              bool status = player["Status"];
+                              players.add(Player(name, status));
+                            });
                           });
                         }
                       },
@@ -102,9 +117,35 @@ class _InvitationState extends State<Invitation> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[Text("Session Link: "), SelectableText(currId)],
-          )
+          ),
+          Container(
+            height: 200,
+            child: ListView.builder(
+                itemCount: players.length,
+                itemBuilder: (context, index) {
+                  return Row(     //TODO implement delete functionality
+                    children: <Widget>[
+                      Icon(
+                        Icons.circle,
+                        color: (players[index].status ? Colors.green : Colors.red),
+                        size: 30.0
+                      ),
+                      Text(players[index].name)
+                    ]
+                  );
+                }
+            )
+          ),
+          ElevatedButton(onPressed:() => null, child: Text("Play"))
         ]),
       ),
     );
   }
+}
+
+class Player {
+  String name;
+  bool status;
+
+  Player(this.name, this.status);
 }
